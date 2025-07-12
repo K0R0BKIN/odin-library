7b8a632a6e13fe49613c38f9e6e62c9fc629cc4d
Nikita Korobkin <nikita.korobkin.personal@gmail.com>
2025-07-12 21:47:28 +0300

    Simplify Library and Book

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 function Library(books = []) {
+    this.node = document.querySelector("#books");
     this.books = [];
-    this.container = this.render();
 
-    books.forEach(book => book.addToLibrary(this));
+    books.forEach(book => this.addBook(book));
 }
 
-Library.prototype.render = function() {
-    const libraryContainer = document.createElement("div");
-    libraryContainer.id = "library";
-    document.body.appendChild(libraryContainer);
-
-    return libraryContainer;
-}
-
-Library.prototype.addBook = function(book) {
+Library.prototype.addBook = function (book) {
     this.books.push(book);
-    book.container = book.render();
+    this.node.appendChild(book.node);
 };
 
 function Book(title, author, pages, read = false) {
@@ -24,32 +16,24 @@ function Book(title, author, pages, read = false) {
     this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID();
-}
 
-Book.prototype.render = function() {
-    const bookCard = document.createElement("div");
-    bookCard.className = "book-card";
-    bookCard.innerHTML = `
+    this.node = document.createElement("div");
+    this.node.className = "book-card";
+    this.node.innerHTML = `
         <h3>${this.title}</h3>
         <p>Author: ${this.author}</p>
         <p>Pages: ${this.pages}</p>
-        <p>Read: ${this.read ? "Yes" : "No"}</p>
+        <label>
+            Read: <span>${this.read ? "☑" : "☐"}</span>
+        </label>
     `;
-    this.library.container.appendChild(bookCard);
+}
 
-    return bookCard;
-};
-
-Book.prototype.addToLibrary = function(library = library) {
-    this.library = library;
-    this.library.addBook(this);
-};
-
-const exampleBooks = [
+const EXAMPLE_BOOKS = [
     new Book("1984", "George Orwell", 328, true),
     new Book("To Kill a Mockingbird", "Harper Lee", 281, false),
     new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true),
     new Book("Moby Dick", "Herman Melville", 585, false),
 ];
 
-const library = new Library(exampleBooks);
+const library = new Library(EXAMPLE_BOOKS);
